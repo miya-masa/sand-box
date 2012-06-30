a45bab0d19b3663b273be538f2389b3730c3675d
miya-masa <miyauchi0623@gmail.com>
2012-06-30 15:30:44 +0900

    ファイルの変更

--- sample/test/time/test/TimeCaluculatorTest.java
+++ sample/test/time/test/TimeCaluculatorTest.java
@@ -8,35 +8,35 @@ import java.util.Locale;
 
 import org.junit.Test;
 
-import time.calculate.TimeCaluculator;
+import time.calculate.TimeCalculator;
 import time.structure.TimeStructure;
 
 public class TimeCaluculatorTest {
 
 	@Test
 	public void testYear() {
-		TimeCaluculator caluculator = new TimeCaluculator();
+		TimeCalculator caluculator = new TimeCalculator();
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis(0);
 		calendar.add(Calendar.YEAR, 1);
 		compare(calendar,
-				caluculator.caluculateTime(TimeCaluculator.SECOND_PER_YEAH));
+				caluculator.calculateTime(TimeCalculator.SECOND_PER_YEAH));
 		calendar.add(Calendar.YEAR, 3);
 		compare(calendar,
-				caluculator.caluculateTime(TimeCaluculator.SECOND_PER_YEAH * 3
-						+ TimeCaluculator.SECOND_PER_YEAH_LEEP));
+				caluculator.calculateTime(TimeCalculator.SECOND_PER_YEAH * 3
+						+ TimeCalculator.SECOND_PER_YEAH_LEEP));
 	}
 
 	@Test
 	public void testCaluculateTime() {
-		TimeCaluculator caluculator = new TimeCaluculator();
+		TimeCalculator caluculator = new TimeCalculator();
 		Calendar calendar = Calendar.getInstance(Locale.JAPAN);
 		long now = System.currentTimeMillis();
 		calendar.setTimeInMillis(now);
-		compare(calendar, caluculator.caluculateTime(now / 1000));
+		compare(calendar, caluculator.calculateTime(now / 1000));
 		long toolongTime = 1000000000000000000l;
 		calendar.setTimeInMillis(toolongTime);
-		compare(calendar, caluculator.caluculateTime(toolongTime / 1000));
+		compare(calendar, caluculator.calculateTime(toolongTime / 1000));
 
 	}
 
